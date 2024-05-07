5d2db5c6697c52d0e0a272642c4c7dc9ee33fdd8
Hamza <imhamzaaslam@gmail.com>
2024-05-07 14:33:33 +0500

    Revert "Revert "Set project drawer and create api fuctions""
    This reverts commit 772aa84cc34f386c6a5089da9636116ae2604440.

---
+++ resources/js/services/api.js
@@ -0,0 +1,46 @@
+/*
+ * This is the initial API interface
+ * we set the base URL for the API.
+ * This will be used for making authenticated requests.
+ */
+
+import axios from "axios";
+import { useAuthStore } from "../store/auth"
+
+export const apiClient = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+    withCredentials: true, // required to handle the CSRF token
+});
+
+export const generateQueryString = (search, orderBy, orderDirection) => {
+    const queryParams = [];
+
+    if (search) {
+        queryParams.push(`keyword=${search}`);
+    }
+    if (orderBy) {
+        queryParams.push(`orderBy=${orderBy}`);
+    }
+    if (orderDirection) {
+        queryParams.push(`order=${orderDirection}`);
+    }
+
+    return queryParams.length > 0 ? `&${queryParams.join('&')}` : '';
+}
+
+/*
+ * Add a response interceptor
+ */
+apiClient.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    function (error) {
+        const store = useAuthStore()
+
+        if (error.response && [401, 419].includes(error.response.status) && store.user) {
+            store.logout()
+        }
+        return Promise.reject(error);
+    }
+);
