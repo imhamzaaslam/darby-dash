8be1e00518f1a56f17b038ef9afcc50dabc31af1
Hamza <imhamzaaslam@gmail.com>
2024-05-17 16:03:04 +0500

    hide user seeder code

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -45,11 +45,11 @@ class UserSeeder extends Seeder
         $this->addDummyInfo($faker, $demoUser2);
         $demoUser2->assignRole(Role::where('name', 'admin')->first());
 
-        User::factory(20)->create()->each(function ($user) use ($faker) {
-            $this->addDummyInfo($faker, $user);
-            $role = $user->id % 2 === 0 ? Role::where('name', 'project manager')->first() : Role::where('name', 'developer')->first();
-            $user->assignRole($role);
-        });
+        // User::factory(20)->create()->each(function ($user) use ($faker) {
+        //     $this->addDummyInfo($faker, $user);
+        //     $role = $user->id % 2 === 0 ? Role::where('name', 'project manager')->first() : Role::where('name', 'developer')->first();
+        //     $user->assignRole($role);
+        // });
     }
 
     /**
