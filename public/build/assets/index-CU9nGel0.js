a036fa86a7c2ef9529ad6f6bde2eca0a44761e6b
hamza <imhamzaaslam@gmail.com>
2024-11-25 18:15:23 +0500

    upload build

---
+++ public/build/assets/index-CU9nGel0.js
@@ -0,0 +1,1 @@
+import a from"./company-dashboard-ovJaEapH.js";import t from"./super-dashboard-B-GrTbX8.js";import{a9 as s,n,o as r,f as e}from"./main-Dxu0HdUW.js";const i={__name:"index",setup(p){const o=s();return(m,u)=>n(o).isSuperAdmin?(r(),e(t,{key:0})):(r(),e(a,{key:1}))}};export{i as default};
