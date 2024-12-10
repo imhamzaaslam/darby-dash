05aed0351b63855041cc45bb361779b2622c0d00
hamza <imhamzaaslam@gmail.com>
2024-12-10 18:24:35 +0500

    upload build

--- public/build/assets/index-Btx5N11p.js
+++
@@ -1,1 +0,0 @@
-import a from"./company-dashboard-BoSIdbGc.js";import t from"./super-dashboard-DwgBBY6h.js";import{ac as s,n,o as r,f as e}from"./main-DJ8-KKk0.js";const i={__name:"index",setup(p){const o=s();return(c,m)=>n(o).isSuperAdmin?(r(),e(t,{key:0})):(r(),e(a,{key:1}))}};export{i as default};
