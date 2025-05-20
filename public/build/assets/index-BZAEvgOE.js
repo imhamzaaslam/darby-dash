654e0d852a3f3514f265b8e11e06ea15456c36ed
Hamza <imhamzaaslam@gmail.com>
2025-05-20 12:22:49 +0500

    upload build

--- public/build/assets/index-BZAEvgOE.js
+++
@@ -1,1 +0,0 @@
-import o from"./company-dashboard-V3d_oR_f.js";import{ac as s,n as t,o as a,f as e,aL as n}from"./main-DhKth3mo.js";const f={__name:"index",setup(c){const r=s();return(p,u)=>t(r).isSuperAdmin?(a(),e(n,{key:0})):(a(),e(o,{key:1}))}};export{f as default};
