54a0f27d3da7fe80fba735feedc2b4ed5fe5977e
Hamza <imhamzaaslam@gmail.com>
2025-05-20 15:38:03 +0500

    upload build

--- public/build/assets/index-BErcKbR4.js
+++
@@ -1,1 +0,0 @@
-import o from"./company-dashboard-Cs9frf4b.js";import{ac as s,n as t,o as a,f as e,aL as n}from"./main-D90Ov6g3.js";const f={__name:"index",setup(c){const r=s();return(p,u)=>t(r).isSuperAdmin?(a(),e(n,{key:0})):(a(),e(o,{key:1}))}};export{f as default};
