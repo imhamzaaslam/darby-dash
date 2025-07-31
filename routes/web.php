fc4cf759fb80d44a1dc3cd73187d31fd6f0df844
Hamza <imhamzaaslam@gmail.com>
2025-07-31 18:21:44 +0500

    add test function to send email

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Mail;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +19,20 @@ Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
 
 Route::get('last-change', function () {
     return '7/7/2025 07:02 PM';
+});
+
+Route::get('send-test-email', function () {
+    try {
+        ini_set('max_execution_time', 120);
+        
+        Mail::raw('This is a test email.', function ($message) {
+            $message->to('rezoprogrammer@gmail.com')
+                    ->subject('Test Email');
+        });
+        return 'Test email sent successfully!';
+    } catch (\Exception $e) {
+        return 'Error sending email: ' . $e->getMessage();
+    }
 });
 
 Route::get('{any?}', function() {
